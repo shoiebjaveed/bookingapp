16940662868ef74bf1d5f9e80e671b3d2d1329cf
shoiebjaveed <45892207+shoiebjaveed@users.noreply.github.com>
2022-03-05 13:58:22 +0530

    Shows the list of all the users who have applied right below the form

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.g
     {
       let addDiv=document.createElement('div');
   addDiv.className="row";
-  addDiv.innerHTML='<div class="col-sm-4" style="padding: 10px;">'+user_records[i].name+'</div><div class="col-sm-4" style="padding: 10px;">'+ user_records[i].email+'</div>';
+  addDiv.innerHTML='<div class="col-sm-4" style="padding: 2px;">'+user_records[i].name+'</div><div class="col-sm-4" style="padding: 2px;">'+ user_records[i].email+'</div>';
   document.getElementById("showUsers").appendChild(addDiv);
 
     }
